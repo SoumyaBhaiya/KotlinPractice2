d6d1aa3bb3866cc28070f2b3ff40b4bb434df8b8
Soumya Bhaiya <soumybhaiya@gmail.com>
2025-05-26 09:10:21 -0700

    Getting started with getters and setters

---
+++ src/Kotlin5.kt
@@ -0,0 +1,17 @@
+// Getting started with getters and setters. Now getters and setters are used to modify the variables.
+// read and change variables
+/*
+    The most basic getter and setter (default is)
+
+    get() = field
+    set(value){
+    field = value
+    }
+
+
+Getter: What to do when someone says "Show me the value."
+
+Setter: What to do when someone says "Change the value."
+
+ */
+
